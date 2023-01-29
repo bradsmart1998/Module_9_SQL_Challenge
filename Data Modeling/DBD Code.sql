ecf9b172bf796802ffcfcd0a467dbc4a80787df7
Brad Smart <bradsmart@live.com>
2023-01-29 21:06:08 +0000

    adding work Brad Smart

--- Data Modeling/DBD Code.sql
+++ Data Modeling/DBD Code.sql
@@ -12,28 +12,22 @@ CREATE TABLE "departments" (
 
 CREATE TABLE "dept_emp" (
     "emp_no" INT   NOT NULL,
-    "dept_no" VARCHAR(255)   NOT NULL,
-    CONSTRAINT "pk_dept_emp" PRIMARY KEY (
-        "emp_no"
-     )
+    "dept_no" VARCHAR(255)   NOT NULL
 );
 
 CREATE TABLE "dept_manager" (
     "dept_no" VARCHAR(255)   NOT NULL,
-    "emp_no" INT   NOT NULL,
-    CONSTRAINT "pk_dept_manager" PRIMARY KEY (
-        "dept_no"
-     )
+    "emp_no" INT   NOT NULL
 );
 
 CREATE TABLE "employees" (
     "emp_no" INT   NOT NULL,
     "emp_title_id" VARCHAR(255)   NOT NULL,
-    "birth_date" DATE   NOT NULL,
+    "birth_date" VARCHAR(255)   NOT NULL,
     "first_name" VARCHAR(255)   NOT NULL,
     "last_name" VARCHAR(255)   NOT NULL,
     "sex" VARCHAR(255)   NOT NULL,
-    "hire_date" DATE   NOT NULL,
+    "hire_date" VARCHAR(255)   NOT NULL,
     CONSTRAINT "pk_employees" PRIMARY KEY (
         "emp_no"
      )
@@ -41,10 +35,7 @@ CREATE TABLE "employees" (
 
 CREATE TABLE "salaries" (
     "emp_no" INT   NOT NULL,
-    "salary" INT   NOT NULL,
-    CONSTRAINT "pk_salaries" PRIMARY KEY (
-        "emp_no"
-     )
+    "salary" INT   NOT NULL
 );
 
 CREATE TABLE "titles" (
